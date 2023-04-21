fd1e4117d229500a013adeda95c6224df2bfbae9
WasinYokk <bo22313@bristol.ac.uk>
2023-04-21 10:24:16 +0100

    Deploy Contract

---
+++ hardhat.config.js
@@ -0,0 +1,24 @@
+//require("@nomicfoundation/hardhat-toolbox");
+
+// Go to https://infura.io, sign up, create a new API key
+// in its dashboard, and replace "KEY" with it
+const INFURA_API_KEY = "02eecf777b174b89b4f98660e588fcfe";
+
+// Replace this private key with your Sepolia account private key
+// To export your private key from Coinbase Wallet, go to
+// Settings > Developer Settings > Show private key
+// To export your private key from Metamask, open Metamask and
+// go to Account Details > Export Private Key
+// Beware: NEVER put real Ether into testing accounts
+const SEPOLIA_PRIVATE_KEY = "793879cab82981c20f69ca346f5cd3562b735586b3b73c4858ddc39bd985d218";
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.18",
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [SEPOLIA_PRIVATE_KEY]
+    }
+  }
+};
