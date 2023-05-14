29d7f6f844e5797f56afa6650fd2e222ba329e0c
WasinYokk <bo22313@bristol.ac.uk>
2023-05-14 18:33:42 +0100

    Update New Function

--- contracts/CrowdFunding.sol
+++ contracts/CrowdFunding.sol
@@ -21,8 +21,9 @@ contract Crowdfunding {
         mapping(uint => Voting) votes;
         uint256 status;
         uint256 current_stage;
-        uint256 start_timestamp;
+        // uint256 start_timestamp;
         uint256 profit; // profit in the end
+        string img;
     }
     
 
@@ -46,10 +47,9 @@ contract Crowdfunding {
         uint256 _stage0time,
         uint256 _stage1time,
         uint256 _stage2time,
-        uint256 _stage3time
+        uint256 _stage3time,
+        string memory _img
     ) public returns (uint256) {
-
-        uint256 minute = 1 minutes;
 
         Campaign storage campaign = campaigns[numberOfCampaigns];
 
@@ -58,8 +58,8 @@ contract Crowdfunding {
         campaign.owner = msg.sender;
         campaign.title = _title;
         campaign.description = _description;
-        campaign.target = _target * 1 ether;
-        campaign.deadline = (_deadline * minute) + block.timestamp;
+        campaign.target = _target;
+        campaign.deadline = _deadline;
         campaign.amountCollected = 0;
         //campaign.amountleft = 0;
         campaign.expectedInterestRate = _expectedInterestRate;
@@ -68,15 +68,11 @@ contract Crowdfunding {
         // int256[] memory timer = new uint256[](numberOfCampaigns);
         campaign.timer = [_stage0time,_stage1time,_stage2time,_stage3time];
         campaign.profit = 0;
+        campaign.img = _img;
 
         numberOfCampaigns++;
 
         return numberOfCampaigns - 1;
-    }
-
-    function publicCampaign(uint256 projectid) public {
-        require(msg.sender == campaigns[projectid].owner, "Only the owner of the campaign can public the project.");
-        campaigns[projectid].status = 1;
     }
 
     function donateToCampaign(uint256 _id) public payable {
@@ -93,11 +89,15 @@ contract Crowdfunding {
             }
 
             campaign.donations[msg.sender] += amount;
+
+            user_campaign_lists[msg.sender].push(_id);
+
             (bool sent, ) = payable(address(this)).call{value: amount}("");
             campaign.amountCollected += amount;
         }else{
             donateRestMoney(_id);
         }
+
 
     }
 
@@ -165,12 +165,12 @@ contract Crowdfunding {
                 require(sent, "Failed to send Ether.");
             }
         }else if (_stage==3){                                                  //release all the profits. call this only one time
-            uint256 profit = campaign.profit;
+            uint256 interestRate = campaign.expectedInterestRate;
             uint256 total_payout = 0;
             for (uint256 i = 0; i < campaign.donators.length; i++) {
                 address donator = campaign.donators[i];
                 uint256 donation = campaign.donations[donator];
-                uint256 payout =  profit * donation / campaign.target;
+                uint256 payout =  ((interestRate+100)/100) * donation;
                 (bool sent, ) =payable(donator).call{value: payout}("");
                 require(sent, "Failed to send Ether.");
                 total_payout += payout;
@@ -179,7 +179,6 @@ contract Crowdfunding {
             // check if all the funds are released
             // check to prevent failing to send money halfway in loop
             // This require is not necessary, just in case
-            require(profit == total_payout, "Not all funds are released");
         }
 
     }
@@ -242,7 +241,53 @@ contract Crowdfunding {
         }
         return (campaigns[_id].donators,amount);  // TODO
     }
-    
+
+    function getCampaignsDetailsImage() public view returns(
+        string[] memory,
+        uint256[] memory,
+        uint256[] memory,
+        uint256[] memory,
+        uint256[] memory,
+        uint256[] memory,
+        uint256[] memory,
+        uint256[] memory
+
+    ){
+        string[] memory imgs = new string[](numberOfCampaigns);
+        uint256[] memory interests = new uint256[](numberOfCampaigns);
+        uint256[] memory stage0time = new uint256[](numberOfCampaigns);
+        uint256[] memory stage1time = new uint256[](numberOfCampaigns);
+        uint256[] memory stage2time = new uint256[](numberOfCampaigns);
+        uint256[] memory stage3time = new uint256[](numberOfCampaigns);
+        uint256[] memory amountLefts = new uint256[](numberOfCampaigns);
+        uint256[] memory profits = new uint256[](numberOfCampaigns);
+
+
+        for (uint256 i = 0; i < numberOfCampaigns; i++) {
+            Campaign storage item = campaigns[i];
+
+            imgs[i] = item.img;
+            interests[i] = item.expectedInterestRate;
+            stage0time[i] = item.timer[0];
+            stage1time[i] = item.timer[1];
+            stage2time[i] = item.timer[2];
+            stage3time[i] = item.timer[3];
+            amountLefts[i] = item.amountleft;
+            profits[i] = item.profit;
+
+        }
+        return (
+            imgs,
+            interests,
+            stage0time,
+            stage1time,
+            stage2time,
+            stage3time,
+            amountLefts,
+            profits
+        );
+    }
+
     function getCampaignsDetails() public view returns (
         address[] memory,
         string[] memory,
@@ -252,6 +297,7 @@ contract Crowdfunding {
         uint256[] memory,
         uint256[] memory,
         uint256[] memory
+        // string[] memory
     ) {
         address[] memory owners = new address[](numberOfCampaigns);
         string[] memory titles = new string[](numberOfCampaigns);
@@ -261,6 +307,7 @@ contract Crowdfunding {
         uint256[] memory amountCollecteds = new uint256[](numberOfCampaigns);
         uint256[] memory statuses = new uint256[](numberOfCampaigns);
         uint256[] memory stages = new uint256[](numberOfCampaigns);
+        // string[] memory images = new string[](numberOfCampaigns);
 
         for (uint256 i = 0; i < numberOfCampaigns; i++) {
             Campaign storage item = campaigns[i];
@@ -273,6 +320,7 @@ contract Crowdfunding {
             amountCollecteds[i] = item.amountCollected;
             statuses[i] = item.status;
             stages[i] = item.current_stage;
+            // images[i] = item.img;
         }
 
         return (
@@ -284,6 +332,7 @@ contract Crowdfunding {
             amountCollecteds,
             statuses,
             stages
+            // images
         );
     }
 
@@ -297,7 +346,166 @@ contract Crowdfunding {
         else if(campaigns[id].status == 6){return("Give Fund Back to Donators");}
         else{return("Status is not in the range");}
     }
-//     If (status == 1 ) public returns{
-// string _status = “raising fund”;
-// return _status;
+    
+
+    mapping(address => uint256[]) user_campaign_lists;
+
+
+    // return the campaign that the donator can vote 
+    function getUserCampaign(address useraddr) public view returns(uint256 [] memory){
+        return user_campaign_lists[useraddr];
+    }
+
+    function getDonateCampaignDetail(address userAdd) public view returns(
+        string[] memory,
+        string[] memory,
+        uint256[] memory,
+        uint256[] memory,
+        uint256[] memory,
+        uint256[] memory,
+        uint256[] memory
+    ){
+        uint256[] memory projectList = getUserCampaign(userAdd);
+
+        string[] memory titles = new string[](numberOfCampaigns);
+        string[] memory descriptions = new string[](numberOfCampaigns);
+        uint256[] memory targets = new uint256[](numberOfCampaigns);
+        uint256[] memory deadlines = new uint256[](numberOfCampaigns);
+        uint256[] memory amountCollecteds = new uint256[](numberOfCampaigns);
+        uint256[] memory statuses = new uint256[](numberOfCampaigns);
+        uint256[] memory stages = new uint256[](numberOfCampaigns);
+
+        for (uint256 i = 0; i < projectList.length; i++) {
+            Campaign storage item = campaigns[i];
+
+            titles[i] = item.title;
+            descriptions[i] = item.description;
+            targets[i] = item.target;
+            deadlines[i] = item.deadline;
+            amountCollecteds[i] = item.amountCollected;
+            statuses[i] = item.status;
+            stages[i] = item.current_stage;
+        }
+
+         return (
+            titles,
+            descriptions,
+            targets,
+            deadlines,
+            amountCollecteds,
+            statuses,
+            stages
+            // images
+        );
+    }
+
+
+    function isFundsEnough(uint256 id)public view returns (bool){
+        // check time before the deadline
+        // require(block.timestamp >= campaigns[id].deadline,"not reach the money raising deadline");
+        return campaigns[id].target <= campaigns[id].amountCollected;
+    }
+
+    function moveNextStage(uint256 project_id) public {
+
+        uint256 stage = campaigns[project_id].current_stage;
+
+        if((stage ==0)&& (campaigns[project_id].status==1)){
+            campaigns[project_id].amountleft = campaigns[project_id].amountCollected;
+            if(block.timestamp >= campaigns[project_id].deadline){
+                if (isFundsEnough(project_id)){                    // fund is enough
+                    // start the project
+                    campaigns[project_id].status = 3;
+                    // withdraw first stage amount of funds
+                    withdrawFunds(project_id, stage);       //
+                    // start voting 
+                    // record the start time
+                    // campaigns[project_id].start_timestamp = block.timestamp;
+                }else{
+                    // project fail at raising money
+                    campaigns[project_id].status = 2;
+                    // release all the funds collected
+                    releaseFunds(project_id,stage);
+                }
+            }   
+        }else if((stage < 3)&& (campaigns[project_id].status==3)){  // stage 0,1,2 voting
+            // uint256 time = campaigns[project_id].start_timestamp;
+            // for (uint256 i = 0;i<=stage;i++){
+            //     time += campaigns[project_id].timer[i]* 1 seconds;     //TODO: change minutes to days
+            // }
+            uint256 i = campaigns[project_id].current_stage;
+            require(block.timestamp > campaigns[project_id].timer[i],"voting has not reached the deadline");
+            if(getVotingResult(project_id, stage)){
+                // project contine and withdraw the part of funds to project owner
+                campaigns[project_id].current_stage += 1;
+                stage = campaigns[project_id].current_stage;
+                withdrawFunds(project_id, stage);
+            }else{
+                // vote to end the project and release the rest of funds
+                campaigns[project_id].status = 4;
+                releaseFunds(project_id,stage);
+            }
+        }else if((stage == 3)&&(campaigns[project_id].status==3)){   // last stage, no votings
+            require(block.timestamp>=campaigns[project_id].deadline,"stage 3 has not reached the deadline");
+            campaigns[project_id].status = 5;
+
+        }else if((stage == 3)&&campaigns[project_id].status==5){     // release funds after project finish and with interest rate
+
+            // require after owner add the profit to campaign
+            uint256 expect = campaigns[project_id].target*(campaigns[project_id].expectedInterestRate/100)+campaigns[project_id].target;
+            if(campaigns[project_id].profit < expect){    // if profit is less than owner promised
+                // add owner to blacklist
+                punish(campaigns[project_id].owner);
+            }
+            // then release the fund 
+            releaseFunds(project_id, stage);
+            campaigns[project_id].status = 6;
+        }
+    }
+
+    function loopMoveNextStage() public {
+        for (uint256 i=0; i < numberOfCampaigns; i++){
+            moveNextStage(i);
+        }
+    }
+
+    function stage_vote(uint256 project_id) public {
+        uint256 stage = campaigns[project_id].current_stage;
+
+        require(!campaigns[project_id].votes[stage].voterAddrs[msg.sender], "Address has already voted.");
+        //check donators address
+        require(isDonator(project_id, msg.sender), "The sender is not a donator.");
+
+        campaigns[project_id].votes[stage].count += campaigns[project_id].donations[msg.sender];
+        campaigns[project_id].votes[stage].voterAddrs[msg.sender] = true;
+    }
+
+    function writeComment(uint256 id, string memory cmt)public {
+        require(msg.sender == campaigns[id].owner,"only owner can set the comment");
+        campaigns[id].votes[campaigns[id].current_stage].comment = cmt;
+    }
+
+    function getComment(uint256 id,uint256 stage) public view returns (string memory){
+        return campaigns[id].votes[stage].comment;
+    }
+
+
+    function isDonator(uint256 project_id, address user) public view returns (bool) {
+        Campaign storage campaign = campaigns[project_id];
+        for (uint256 i = 0; i < campaign.donators.length; i++) {
+            if (campaign.donators[i] == user) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function getVotingResult(uint256 project_id, uint256 stage) public view returns (bool) {
+        // require donators to view the voting results
+        // require(isDonator(project_id, msg.sender), "The sender is not a donator.");
+        // require(msg.sender == campaigns[project_id].owner, "Only the owner of the campaign can public the project.");
+        bool vote_result = (campaigns[project_id].votes[stage].count > campaigns[project_id].target / 2);
+        return vote_result;
+    }
+
 }
